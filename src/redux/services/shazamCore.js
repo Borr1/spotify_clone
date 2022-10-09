952f3618ab3b2191e444fbccaa00f5e4ee76407f
Borr1 <borhen.benletaief@gmail.com>
2022-10-09 17:01:38 +0100

    fetched data from api

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        'ff2a4ebc6emsh34a59165f8d49fap1d793bjsn6e1a4c16424d',
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: '/chars/world' }),
+  }),
+});
+
+export const { useGetTopCharsQuery } = shazamCoreApi;
