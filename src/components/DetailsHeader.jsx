cace757257e596908482fffe538b038ba1cc55f1
Borr1 <borhen.benletaief@gmail.com>
2022-10-11 23:18:00 +0100

    added song details page and artist details page

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,20 +1,39 @@
 import { Link } from 'react-router-dom';
-const DetailsHeader = ({ artistId, artistData, songData }) => (
-  <div className="relative w-full flex flex-col">
-    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
-    <div className="absolute inset-0 flex items-center">
-      <img
-        src={
-          artistId
-            ? artistData?.artists[artistId]?.attributes?.artwork?.url
-                .replace('{w}', '500')
-                .replace('{h}', '500')
-            : songData?.images?.coverart
-        }
-        alt="art"
-      />
+
+const DetailsHeader = ({ artistId, artistData, songData }) => {
+  const artist = artistData?.artists[artistId]?.attributes;
+  return (
+    <div className="relative w-full flex flex-col">
+      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+      <div className="absolute inset-0 flex items-center">
+        <img
+          className="rounded-full sm:w-48 w-28 sm:h-48 h-28 object-cover shadow-xl border-2 shadow-black"
+          src={
+            artistId
+              ? artist.artwork?.url.replace('{w}', '500').replace('{h}', '500')
+              : songData?.images?.coverart
+          }
+          alt="art"
+        />
+        <div className="ml-5">
+          <p className="font-bold text-white text-xl sm:text-3xl">
+            {artistId ? artist.name : songData?.title}
+          </p>
+          {!artistId && (
+            <Link to={`/artists/${songData?.artists[0].adamid}`}>
+              <p className="text-gray-400 mt-2  text-base">
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+          <p className="text-gray-400 mt-2  text-base">
+            {artistId ? artist?.genreNames[0] : songData?.genres.primary}
+          </p>
+        </div>
+      </div>
+      <div className="w-full sm:h-20 h-24" />
     </div>
-  </div>
-);
+  );
+};
 
 export default DetailsHeader;
--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,30 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import SongBar from './SongBar';
+
+const RelatedSongs = ({
+  handlePauseClick,
+  handlePlayClick,
+  activeSong,
+  isPlaying,
+  data,
+  artistId,
+}) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    <div className="mt-6 w-full flex-col">
+      {data?.map((song, i) => (
+        <SongBar
+          artistId={artistId}
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+        />
+      ))}
+    </div>
+  </div>
 );
 
 export default RelatedSongs;
